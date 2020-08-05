5cb78de401025d7b665966d63237332166de5d24
Gerald Sousa <gerald.sousa@gmail.com>
2020-08-05 10:20:18 -0400

    complete linkedList.js

--- linkedList.js
+++ linkedList.js
@@ -36,10 +36,68 @@ LinkedList.prototype.addToTail = function (value) {
     this.tail = newNode;
 }
 
+LinkedList.prototype.removeHead = function () {
+    // removeHead returns value in current head and removes that node
+    // To remove the head we need to 
+    // check if list is empty
+    if (!this.head) return null;
+    // set head to head.next
+    let val = this.head.value;  // save value of current head
+    this.head = this.head.next;
+
+    // set head.prev to null;
+    if (this.head) this.head.prev = null;
+    else this.tail = null;
+    return val;
+}
+
+LinkedList.prototype.removeTail = function () {
+    // removeTail returns the value in current tail and removes that node
+    // To remove tail we need to 
+    // check to see if list is empty
+    if (!this.tail) return null;
+    // set tail to tail.prev
+    let val = this.tail.value;  // save value of current tail
+    this.tail = this.tail.prev;
+    // set tail.next to null;
+    if (this.tail) this.tail.next = null;
+    else this.head = null;
+    return val;
+
+}
+
+LinkedList.prototype.search = function (searchVal) {
+    // set curNode to head to start
+    let curNode = this.head;
+    // loop through list until curNode === null;
+    while (curNode) {
+        if (curNode.value === searchVal) return curNode.value;
+        curNode = curNode.next;
+    }
+    return -1;
+}
+
+LinkedList.prototype.indexOf = function (searchVal) {
+    // this function will return all of the index for nodes having the value 
+    let myArr = [];
+    let idx = 0
+    // set curNode to head to start search
+    let curNode = this.head;
+    while (curNode) {
+        if (curNode.value === searchVal) myArr.push(idx);
+        idx++;
+        curNode = curNode.next;
+    }
+    return myArr;
+}
+
 let ll = new LinkedList();
 ll.addToHead(100);
 //console.log(ll);
-ll.addToHead(200);
+ll.addToTail(200);
 //console.log(ll);
 ll.addToTail(300);
-console.log(ll.tail.prev.prev);
+ll.addToTail(100);
+ll.addToTail('tail');
+
+console.log(ll.indexOf(100));
