4ed4cb8e82f855b920c6e269370ab7f297e10a23
Gerald Sousa <gerald.sousa@gmail.com>
2020-08-06 15:14:00 -0400

    added getMinVal and getMaxVal prototype methods

--- binaryTree.js
+++ binaryTree.js
@@ -42,23 +42,47 @@ BinaryTree.prototype.contains = function (val) {
 // This version of depthFirstTraversal is also known as In Order traversal
 BinaryTree.prototype.depthFirstTraversal = function (iteratorFunc, order) {
     // depth first traversal will return all values in order
+    if (order === "pre-order") {
+        iteratorFunc(this.value);
+    }
     if (this.left) {
         this.left.depthFirstTraversal(iteratorFunc, order);
     }
-    if (order === "in-order"); {
+    if (order === "in-order") {
         iteratorFunc(this.value);
     }
 
     if (this.right) {
         this.right.depthFirstTraversal(iteratorFunc, order);
     }
-
+    if (order === "post-order") {
+        iteratorFunc(this.value);
+    }
 
 }
 
 BinaryTree.prototype.breadthFirstTraversal = function (iteratorFunc) {
     // breath first traversal will return all values in order of depth from head 
+    let queue = [this];  // used to store entries from each level starting with root node [this]
+    while (queue.length) {
+        let treeNode = queue.shift();   // pops first entry of queue and stores in variable
+        iteratorFunc(treeNode);
+        if (treeNode.left) queue.push(treeNode.left);
+        if (treeNode.right) queue.push(treeNode.right);
+    }
+}
 
+BinaryTree.prototype.getMinVal = function () {
+    // since the minimum value in the tree will be in the lowest left hand child
+    // we only want to stay left until we hit bottom
+    if (this.left) return this.left.getMinVal();
+    return this.value;
+}
+
+BinaryTree.prototype.getMaxVal = function () {
+    // just looking for bottom right child
+    if (this.right) return this.right.getMaxVal();
+    return this.value;
 }
 
 
@@ -76,16 +100,20 @@ BT.insert(35)
 BT.insert(85)
 BT.insert(105)
 BT.insert(10)
+BT.insert(5);
+BT.insert(199);
 //console.log(BT.right.right);
 //console.log(BT.contains(11));
 
-BT.depthFirstTraversal(log, "in-order");
+//BT.depthFirstTraversal(log, "post-order");
 
-function log(val) {
-    console.log(val);
+function log(node) {
+    console.log(node.value);
 }
 
-
+// BT.breadthFirstTraversal(log);
+console.log(BT.getMinVal());
+console.log(BT.getMaxVal());
 // // Factorial Exmaple
 // function factorial(x) {
 //     if (x === 1) {  // base case
