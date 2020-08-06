dc05c223a7286a2c318f78b9f022fc274c1a7219
Gerald Sousa <gerald.sousa@gmail.com>
2020-08-06 11:17:52 -0400

    Added contains prototype method

--- binaryTree.js
+++ binaryTree.js
@@ -1,42 +1,68 @@
 // This module will review the creation and use of the binary tree data structure
 // in Javascript.  We get to use recursion for this one...Yeah
 
-function Node(value, left, right) {
-    this.value = value;
-    this.left = left;
-    this.left = right;
+// You can think of a binary tree as a hiararchy of binary trees (recursive I know)
+
+function BinaryTree(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
 }
 
-function BinaryTree() {
-    this.head = null;
-}
-
-BinaryTree.prototype.addNode = function (val) {
-    // Creates an new node, then adds the node in the tree in the correct place
-    let newNode = new Node(val, null, null);
-    // if tree is empy simply create node and set left and right to null;
-    if (!this.head) this.head = newNode;
-
-}
-
-BinaryTree.prototype.search = function (start, val) {
-
-}
-
-let BT = new BinaryTree();
-
-
-console.log(BT);
-BT.addNode(20);
-console.log(BT);
-
-// Factorial Exmaple
-function factorial(x) {
-    if (x === 1) {  // base case
-        return x;
-    } else {  // recursive case
-        return x * factorial(x - 1);
+BinaryTree.prototype.insert = function (val) {
+    // if new val is <= than current node move down and left
+    if (val <= this.value) {
+        // only insert new tree if left branch is empty
+        if (!this.left) this.left = new BinaryTree(val);
+        else this.left.insert(val);     // recursively call insert() on left branch
+    } else if (val > this.value) {  // move down and right
+        if (!this.right) this.right = new BinaryTree(val);
+        else this.right.insert(val);  //recursively call insert on right branch
     }
+
 }
 
-console.log(factorial(4));
+BinaryTree.prototype.contains = function (val) {
+    if (val === this.value) return true;
+    if (val < this.value) {
+        if (!this.left) return false;
+        else return this.left.contains(val);
+    } else if (val > this.value) {
+        if (!this.right) return false;
+        else return this.right.contains(val);
+    }
+    return false;
+}
+
+
+
+let BT = new BinaryTree(50);
+
+
+BT.insert(30);
+BT.insert(70);
+BT.insert(60);
+BT.insert(100);
+BT.insert(59)
+BT.insert(20)
+BT.insert(45)
+BT.insert(35)
+BT.insert(85)
+BT.insert(105)
+BT.insert(10)
+//console.log(BT.right.right);
+console.log(BT.contains(11));
+
+
+
+
+// // Factorial Exmaple
+// function factorial(x) {
+//     if (x === 1) {  // base case
+//         return x;
+//     } else {  // recursive case
+//         return x * factorial(x - 1);
+//     }
+// }
+
+// console.log(factorial(4));
